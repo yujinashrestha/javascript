c2f033d1f0a05f3177f874944cd2844995c1801e
yujinashrestha <125184869+yujinashrestha@users.noreply.github.com>
2025-02-12 12:03:31 +0000

    commit

--- 01_basics/scope.js
+++ 01_basics/scope.js
@@ -19,12 +19,42 @@
 
 // one()
 
-const func=()=>{
-    console.log("this is the arrow function")
+// const func=()=>{
+//     console.log("this is the arrow function")
+// }
+
+// func()
+
+// const add=(a , b)=>({username:"yujina"})
+
+// console.log(add(5, 3))
+
+// const addtwo=function(a,b){
+//     console.log(a+b)
+// }
+// addtwo(4, 5)
+
+// const user={
+//     username:"hitesh",
+//     gmail:"hitest@google.com",
+//     message:function(){
+//       console.log(`${this.username}, welcome to this website !! `)
+//     }
+// }
+
+// user.message()
+
+// function func(){
+//     let username="yujina"
+//     console.log(`${this.username}, welcome !! `)
+// }
+
+// func()
+const func=(a, b)=>{
+  return a+b
 }
+console.log(func(1,2))
 
-func()
+const func1=(c, d)=>({username:"yujina"})
 
-const add=(a , b)=>({username:"yujina"})
-
-console.log(add(5, 3))
+console.log(func1(101, 3))
